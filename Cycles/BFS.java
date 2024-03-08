be46fe62a8ad1542b0e2e526ecfa48483f78ef2d
Ixhika <106822722+Ixhika@users.noreply.github.com>
2024-03-08 19:19:10 +0530

    up

---
+++ Cycles/BFS.java
@@ -0,0 +1,63 @@
+package Cycles;
+/*Given a directed graph. The task is to do Breadth First Traversal of this graph starting from 0.
+Note: One can move from node u to node v only if there's an edge from u to v. Find the BFS traversal of the graph starting from the 0th vertex, from left to right according to the input graph. Also, you should only take nodes directly or indirectly connected from Node 0 in consideration.
+
+
+Example 1:
+
+Input:
+V = 5, E = 4
+adj = {{1,2,3},{},{4},{},{}}
+
+
+Output: 
+0 1 2 3 4
+Explanation: 
+0 is connected to 1 , 2 , 3.
+2 is connected to 4.
+so starting from 0, it will go to 1 then 2
+then 3. After this 2 to 4, thus bfs will be
+0 1 2 3 4.
+Example 2:
+
+Input:
+V = 3, E = 2
+adj = {{1,2},{},{}}
+
+Output: 
+0 1 2
+Explanation:
+0 is connected to 1 , 2.
+so starting from 0, it will go to 1 then 2,
+thus bfs will be 0 1 2.  */
+
+import java.util.*;
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+public class BFS {
+    public ArrayList<Integer> bfsOfGraph(int V, ArrayList<ArrayList<Integer>> adj){
+        ArrayList<Integer> res = new ArrayList<>();
+
+        Queue<Integer> q = new LinkedList<>();
+        q.add(0);
+
+        boolean[] visited = new boolean[V];
+        visited[0] = true;
+
+        while(!q.isEmpty()){
+            int len = q.size();
+            int curr = q.poll();
+
+            res.add(curr);
+            for(int i : adj.get(curr)){
+                if(!visited[i]){
+                    q.add(i);
+
+                    visited[i] = true;
+                }
+            }
+        }
+        return res;
+    }
+}
